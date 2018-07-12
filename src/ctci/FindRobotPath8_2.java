ee36c87f2d4fec128307764ed6a974853fce580e
Christopher M Ray <christopher.michael.ray@ibm.com>
2018-07-12 03:05:35 -0700

    solution from book

--- src/ctci/FindRobotPath8_2.java
+++ src/ctci/FindRobotPath8_2.java
@@ -3,6 +3,7 @@ package ctci;
 import java.awt.Point;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Random;
 
@@ -10,34 +11,46 @@ public class FindRobotPath8_2 {
 	
 	static List<Point> findPath(Boolean[][] grid){
 		if(grid == null || grid.length ==0) return null;
-		return findPath(grid, grid.length-1, grid[0].length-1, new ArrayList<Point>());
-		
-	}
-	
-	static List<Point> findPath(Boolean[][] grid, int r, int c, List<Point> path){
-		if(r < 0 || c<0 || !grid[r][c]) {
-			return null;
-		}
-		Boolean isAtOrigin = (r==0 && c==0);
-		if(isAtOrigin || findPath(grid, r, c-1, path) != null || findPath(grid, r-1, c, path) != null) {
-			path.add(new Point(r,c));
+		HashMap<Point, Boolean> cache = new HashMap<Point, Boolean>();
+		ArrayList<Point> path = new ArrayList<Point>();
+		if(findPath(grid, grid.length-1, grid[0].length-1, path, cache)) {
 			return path;
 		}
 		else {
-			return null;	
-			
+			return null;
 		}
+		
+	}
+	
+	static Boolean findPath(Boolean[][] grid, int r, int c, ArrayList<Point> path, HashMap<Point,Boolean> cache){
+		if(r < 0 || c<0 || !grid[r][c]) {
+			return false;
+		}
+	
+		Point p = new Point(r,c);
+		if(cache.containsKey(p)) {
+			return cache.get(p);
+		}
+		
+		boolean success = false;
+		
+		if((r==0 && c==0) || findPath(grid, r, c-1, path, cache) || findPath(grid, r-1, c, path, cache)) {
+			path.add(p);
+			success = true;
+		}
+		cache.put(p, success); 
+		return success;
 		
 	}
 
 	public static void main(String[] args) { 
 
 	    Random random = new Random(); 
-	    int size = 5;
-		Boolean[][] grid = new Boolean[size][size];
+	    int size = 10;
+		Boolean[][] grid = new Boolean[size][size-3];
 		for(int i=0; i< size; i++) {
-			for(int j =0; j< size; j++) {
-				if(i ==0 && j == 0 || i == size-1 && j ==size-1)
+			for(int j =0; j< size-3; j++) {
+				if(i ==0 && j == 0 || i == size-1 && j ==size-3)
 					grid[i][j] = true;
 				else
 					grid[i][j] = random.nextBoolean();
