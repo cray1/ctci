131a13c874c9048af7db429e510eddbad85816b7
Christopher M Ray <christopher.michael.ray@ibm.com>
2018-07-16 06:07:58 -0700

    use hashset

--- src/ctci/PowerSet_8_4.java
+++ src/ctci/PowerSet_8_4.java
@@ -2,8 +2,8 @@ package ctci;
 
 import java.util.HashSet;
 import java.util.Iterator;
-import java.util.List;
-import java.util.ArrayList;
+import java.util.Set;
+import java.util.HashSet;
 import java.util.Collection;
 import java.util.HashMap;
 
@@ -15,53 +15,53 @@ public class PowerSet_8_4 {
 	
 	
 	
-	static <T> List<T> convertIntToSubset(int num, List<T> list){
-		List<T> returnList = new ArrayList<T>();
+	static <T> Set<T> convertIntToSubset(int num, Set<T> Set){
+		Set<T> returnSet = new HashSet<T>();
 		int i =num;
-		Iterator<T> itr = list.iterator();
+		Iterator<T> itr = Set.iterator();
 		while(itr.hasNext()) {
 			T next = (T) itr.next();
 			if((i&1) ==1) {
-				returnList.add(next);
+				returnSet.add(next);
 			}
 			i >>= 1;
 		}
-		return returnList;
+		return returnSet;
 	}
 	
-	public static <T> List<List<T>> powerset(List<T> list) {
-		  List<List<T>> listOfSets = new ArrayList<List<T>>();
-		  int max = (1 << list.size())-1;
+	static <T> Set<Set<T>> powerset(Set<T> Set) {
+		  Set<Set<T>> SetOfSets = new HashSet<Set<T>>();
+		  int max = (1 << Set.size())-1;
 		  for(int i=0; i<max; i++) {
-			  List<T> subsets = convertIntToSubset(i, list);
-			  listOfSets.add(subsets);
+			  Set<T> subsets = convertIntToSubset(i, Set);
+			  SetOfSets.add(subsets);
 		  }
-		  return listOfSets;
+		  return SetOfSets;
 		  
 		}
 	
-	static <T> String listToString(List<T> list) {
+	static <T> String SetToString(Set<T> Set) {
 		StringBuilder sb = new StringBuilder();
-		for(T item: list) {
+		for(T item: Set) {
 			sb.append(item.toString() +","); 
 		}
 		return sb.toString();
 	}
 	
-	static <T> void printList(List<List<T>> list) {
-		for(List<T> set: list) {
-			System.out.println(listToString(set));
+	static <T> void printSet(Set<Set<T>> Set) {
+		for(Set<T> set: Set) {
+			System.out.println(SetToString(set));
 		}
 	}
 
 	public static <T> void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		List<Character> s1 = new ArrayList<Character>();
+		Set<Character> s1 = new HashSet<Character>();
 		s1.add('a'); s1.add('b'); s1.add('c'); s1.add('d');
 		
-		List<List<Character>> ps = powerset(s1);
-		printList(ps);
+		Set<Set<Character>> ps = powerset(s1);
+		printSet(ps);
 	}
 
 }
